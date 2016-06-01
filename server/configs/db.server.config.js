b8828ec612bb3956f2a0f2d15119257d8d99958b
Emi Gerez <Emi Gerez>
2016-05-31 23:44:12 -0300

    change db url

--- server/configs/db.server.config.js
+++ server/configs/db.server.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/jwt'
+    devUrl: 'mongodb://localhost:27017/jwt',
+    url: 'mongodb://adminDB:admin1234@ds013891.mlab.com:13891/dbfortest',
 };
